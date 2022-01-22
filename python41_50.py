2daa7127670250d7e0d3ebb88b0c4134218986ac
Sangjin <xmfl34@naver.com>
2022-01-23 07:17:30 +0900

    commit

---
+++ python41_50.py
@@ -0,0 +1,23 @@
+# 41 
+ticker = "btc_krw"
+ticker_a = ticker.upper()
+print(ticker_a)
+
+
+# 42
+ticker1 = "BIT_KRW"
+ticker1_a = ticker.lower()
+print(ticker1_a)
+
+
+# 43
+print('hello'.capitalize())
+
+
+# 44
+# endswith (특정 문자 찾기, 특정 문자로 시작 또는 끝나는 문자열)
+file_name = "report123.xlsx"
+file_name2 = "pythontest.xlsx"
+
+print(file_name.endswith("xlsx"))
+print(file_name2.endswith("xls"))
